1e4bd1c1787ec47a83a26509159d35e91bfb9e15
igor <igorc7871@gmail>
2026-05-06 17:29:02 -0300

    Fix: Corrigindo o usuario repositorio

--- LojaEcomerce/Repositorio/UsuarioRepositorio.cs
+++ LojaEcomerce/Repositorio/UsuarioRepositorio.cs
@@ -1,8 +1,10 @@
 ﻿using LojaEcomerce.Interfaces;
 using LojaEcomerce.Models;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Generators;
 using System.Security.Cryptography.X509Certificates;
-
+using BCrypt.Net;
+using System.IO.Pipelines;
 namespace LojaEcomerce.Repositorio
 {
     public class UsuarioRepositorio : IUsuarioRepositorio
@@ -22,24 +24,55 @@ namespace LojaEcomerce.Repositorio
             using var conn = new MySqlConnection(_connectionString);
             conn.Open();
 
-            var sql = " SELECT * FROM tb_Usuario WHERE Email= @email AND Senha= @senha ";
+            var sql = " SELECT * FROM tb_Usuario WHERE Email= @email ";
             var cmd = new MySqlCommand(sql, conn);
             cmd.Parameters.AddWithValue("@email", Email);
-            cmd.Parameters.AddWithValue("@senha", Senha);
+         
 
             using var reader = cmd.ExecuteReader();
             if (reader.Read())
             {
-                return new LoginViewModel
+                string SenhaBanco = reader["Senha"].ToString();
+
+                if (BCrypt.Net.BCrypt.Verify(Senha, SenhaBanco))
                 {
-                    Id = Convert.ToInt32(reader["Id"]),
-                    Nome = reader["Nome"].ToString()!,
-                    Email = reader["Email"].ToString()!,
-                    Nivel = reader["Nivel"].ToString()!
-                };
+                    return new LoginViewModel
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Nome = reader["Nome"].ToString()!,
+                        Email = reader["Email"].ToString()!,
+                        Nivel = reader["Nivel"].ToString()!
+
+                    };
+                }
+
+
             }
             return null;
 
         }
+
+        public void CriarConta(LoginViewModel usuario)
+        {
+           
+           using (var conn = new MySqlConnection(_connectionString))
+            {
+                //Criptografando a senha antes de enviar ao mysql(Banco)
+                conn.Open();
+                string senhaHash = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
+
+                var sql = "Insert into tb_Usuario(Nome,Email,Senha,Nivel) VALUES(@nome,@email,@senha,@nivel)";
+                var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@email", usuario.Email);
+                cmd.Parameters.AddWithValue("@senha", senhaHash);
+                cmd.Parameters.AddWithValue("@nivel", "Usuario");
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+            
+        
     }
+
 }
