d0bd7c8b5622db28bd520b955ac955d4721417ad
unknown <ms2689992@gmail.com>
2026-05-06 14:24:37 -0300

    feat: Criando a tela login

--- LojaEcomerce/Program.cs
+++ LojaEcomerce/Program.cs
@@ -1,13 +1,18 @@
 using LojaEcomerce.Interfaces;
 using LojaEcomerce.Repositorio;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// injeçao de dependencia 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Usuario/Logar";
+        options.AccessDeniedPath = "/Usuario/AcessoNegado";
+    });
+
+// INJEÇÃO DE DEPENDENCIA
 builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
-
-
-
 
 
 
@@ -25,6 +30,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//MUITO IMPORTANTE COLOCAR
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
